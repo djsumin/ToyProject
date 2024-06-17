3608921b7a6dfc5ca229780208e3d7ce2459a57b
이수민 <judy3504@naver.com>
2024-06-17 17:30:40 +0900

    [init] 프로젝트 생성

---
+++ familia/src/test/java/com/toy/familia/FamiliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.familia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
